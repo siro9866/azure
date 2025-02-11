3ca6b83e67b0ab19c4903686348005f90d505ccf
sangsil <siro9866@gmail.com>
2025-02-11 23:15:20 +0900

    d

---
+++ src/main/java/com/sil/azure/sample/controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.sil.azure.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample/1")
+    public String sample1(){
+        return "Sample 1";
+    }
+
+    @GetMapping("/sample/2")
+    public String sample2(){
+        return "Sample 2";
+    }
+
+
+}
